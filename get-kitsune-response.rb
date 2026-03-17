f398dad89faa350014df03cefcec6f980d707617
Roland Tanglao <roland@rolandtanglao.com>
2026-03-16 20:59:07 -0700

    update getKitsuneResponse

--- get-kitsune-response.rb
+++ get-kitsune-response.rb
@@ -1,22 +1,29 @@
 def getKitsuneResponse(url, params, logger)
-  logger.debug url
-  logger.debug params
+  logger.debug "url in getKitsuneResponse: #{url}"
+  logger.debug "params in getKitsuneResponse: #{params}"
   try_count = 0
   begin
     result = Typhoeus::Request.get(
       url,
       params: params
     )
+    response_code = result.code
+    logger.debug "response_code: #{response_code}"
+
     x = JSON.parse(result.body)
   rescue JSON::ParserError
     try_count += 1
     if try_count < 4
-      $stderr.printf("JSON::ParserError exception, retry:%d\n",\
-                     try_count)
-      sleep(10)
+      logger.debug "JSON::ParserError exception, response code: #{response_code}" \
+       "retry: #{try_count}"
+      if [429, 502, 500].include?(response_code)
+        sleep(60)
+      else
+        sleep(2)
+      end
       retry
     else
-      $stderr.printf("JSON::ParserError exception, retrying FAILED\n")
+      $stderr.printf("JSON::ParserError exception, re-trying FAILED\n")
       x = nil
     end
   end
